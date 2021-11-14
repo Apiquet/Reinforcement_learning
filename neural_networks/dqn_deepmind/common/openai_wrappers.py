70070e0ce5184c8063f8c8d4310d5cb0388224ec
Anthony <anthony.piquet2@gmail.com>
2021-11-14 21:11:58 +0100

    [DEV][ENV] Add wrapper suffix

--- neural_networks/dqn_deepmind/common/openai_wrappers.py
+++ neural_networks/dqn_deepmind/common/openai_wrappers.py
@@ -12,16 +12,17 @@ import numpy as np
 import collections
 
 
-class Res84WithHeightCrop(gym.ObservationWrapper):
+class Res84x84x1Wrapper(gym.ObservationWrapper):
     """Change input resolution from (210, 160, 3) to (84, 84, 1)
     Convert to grayscale with ponderation: r*0.299, g*0.587, b*0.114
     """
     def __init__(self, env=None, skip=4):
-        super(Res84WithHeightCrop, self).__init__(env)
+        super(Res84x84x1Wrapper, self).__init__(env)
 
     def observation(self, obs):
         frame = cv2.resize(
-            obs, (110, 84), interpolation=cv2.INTER_AREA)
+            obs, (110, 84), interpolation=cv2.INTER_AREA).astype(
+                np.float32)
         frame = frame[:, 13:110-13]
         frame = frame[:, :, 0] * 0.299 + frame[:, :, 1] * 0.587 + \
             frame[:, :, 2] * 0.114
