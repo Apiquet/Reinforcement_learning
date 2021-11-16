d91a2e4d40cf0b63057f2491c167ee701dcce142
Anthony <anthony.piquet2@gmail.com>
2021-11-16 20:57:02 +0100

    [DEV][DQN] Change neural net impl

--- neural_networks/dqn_deepmind/models/dqn_2013.py
+++ neural_networks/dqn_deepmind/models/dqn_2013.py
@@ -6,26 +6,28 @@ Deep Q-learning model from: https://arxiv.org/pdf/1312.5602.pdf
 """
 
 from tensorflow.keras.layers import Conv2D, Dense, Flatten
+from tensorflow.keras import Model, Sequential
 
 
-class DQN(nn.Module):
+class DQN(Model):
     def __init__(self, n_input_channels, n_actions):
         super(DQN, self).__init__()
 
-        self.model = nn.Sequential(
+        self.model = Sequential([
             Conv2D(filters=16,
                    kernel_size=(8, 8),
                    activation="relu",
-                   stride=4,
+                   strides=(4, 4),
                    name="Conv1"),
             Conv2D(filters=32,
                    kernel_size=(4, 4),
                    activation="relu",
-                   stride=2,
+                   strides=(2, 2),
                    name="Conv2"),
             Flatten(),
-            Dense(256, activation="relu", name="Dense1")
-            Dense(n_actions, name="Output")
+            Dense(256, activation="relu", name="Dense1"),
+            Dense(n_actions, name="Output")],
+            name="DQN_2013"
         )
 
     def call(self, x):
