9fda043b07cd42fd320b09b7cd4ebef67fad0bef
Anthony <anthony.piquet2@gmail.com>
2022-01-04 19:47:06 +0100

    [DEV] Format

--- neural_networks/dqn_deepmind/common/openai_wrappers.py
+++ neural_networks/dqn_deepmind/common/openai_wrappers.py
@@ -6,10 +6,11 @@ Wrappers for openai gym environments, strongly inspired of
 openai/baselines/blob/master/baselines/common/atari_wrappers.py
 """
 
+import collections
+
 import cv2
 import gym
 import numpy as np
-import collections
 
 
 class ScaledFrom0To1Wrapper(gym.ObservationWrapper):
@@ -21,33 +22,33 @@ class Res84x84x1Wrapper(gym.ObservationWrapper):
     """Change input resolution from (210, 160, 3) to (84, 84, 1)
     Convert to grayscale with ponderation: r*0.299, g*0.587, b*0.114
     """
+
     def __init__(self, env=None, skip=4):
         super(Res84x84x1Wrapper, self).__init__(env)
 
     def observation(self, obs):
-        frame = cv2.resize(
-            obs, (110, 84), interpolation=cv2.INTER_AREA).astype(
-                np.float32)
-        frame = frame[:, 13:110-13]
-        frame = frame[:, :, 0] * 0.299 + frame[:, :, 1] * 0.587 + \
-            frame[:, :, 2] * 0.114
-        assert np.prod(frame.shape) == 84*84*1
+        frame = cv2.resize(obs, (110, 84), interpolation=cv2.INTER_AREA).astype(np.float32)
+        frame = frame[:, 13 : 110 - 13]
+        frame = frame[:, :, 0] * 0.299 + frame[:, :, 1] * 0.587 + frame[:, :, 2] * 0.114
+        assert np.prod(frame.shape) == 84 * 84 * 1
         return frame
+
 
 class StackLast4Wrapper(gym.ObservationWrapper):
     """Stack last 4 frames as input"""
+
     def __init__(self, env):
         super(StackLast4Wrapper, self).__init__(env)
 
     def reset(self):
-        self.buffer = np.zeros(
-            (84, 84, 4), dtype=np.float32)
+        self.buffer = np.zeros((84, 84, 4), dtype=np.float32)
         return self.observation(self.env.reset())
 
     def observation(self, observation):
         self.buffer[:, :, :-1] = self.buffer[:, :, 1:]
         self.buffer[:, :, -1] = observation
         return self.buffer
+
 
 class Skip4FramesAndReturnMaxFrom2FramesWrapper(gym.Wrapper):
     def __init__(self, env):
@@ -71,3 +72,12 @@ class Skip4FramesAndReturnMaxFrom2FramesWrapper(gym.Wrapper):
         obs = self.env.reset()
         self.frames_buffer.append(obs)
         return obs
+
+
+def apply_all_wrappers(env):
+    env = ScaledFrom0To1Wrapper(env)
+    env = Skip4FramesAndReturnMaxFrom2FramesWrapper(env)
+    env = Res84x84x1Wrapper(env)
+    env = StackLast4Wrapper(env)
+    env.reset()
+    return env
--- neural_networks/dqn_deepmind/models/dqn_2013.py
+++ neural_networks/dqn_deepmind/models/dqn_2013.py
@@ -5,29 +5,27 @@
 Deep Q-learning model from: https://arxiv.org/pdf/1312.5602.pdf
 """
 
-from tensorflow.keras.layers import Conv2D, Dense, Flatten
 from tensorflow.keras import Model, Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten
 
 
 class DQN(Model):
     def __init__(self, n_input_channels, n_actions):
         super(DQN, self).__init__()
 
-        self.model = Sequential([
-            Conv2D(filters=16,
-                   kernel_size=(8, 8),
-                   activation="relu",
-                   strides=(4, 4),
-                   name="Conv1"),
-            Conv2D(filters=32,
-                   kernel_size=(4, 4),
-                   activation="relu",
-                   strides=(2, 2),
-                   name="Conv2"),
-            Flatten(),
-            Dense(256, activation="relu", name="Dense1"),
-            Dense(n_actions, name="Output")],
-            name="DQN_2013"
+        self.model = Sequential(
+            [
+                Conv2D(
+                    filters=16, kernel_size=(8, 8), activation="relu", strides=(4, 4), name="Conv1"
+                ),
+                Conv2D(
+                    filters=32, kernel_size=(4, 4), activation="relu", strides=(2, 2), name="Conv2"
+                ),
+                Flatten(),
+                Dense(256, activation="relu", name="Dense1"),
+                Dense(n_actions, name="Output"),
+            ],
+            name="DQN_2013",
         )
         opt = tf.keras.optimizers.Adam(lr=self.policy_learning_rate)
         self.model.compile(optimizer=opt, loss='categorical_crossentropy')
